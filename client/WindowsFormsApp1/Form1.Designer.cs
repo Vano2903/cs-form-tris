3ffe59fe2c708b9f8587c4b5151b4384413987b6
vano <davidevanoncini2003@gmail.com>
2021-05-06 12:53:46 +0200

    gotta go ;-;

---
+++ client/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.genGame = new System.Windows.Forms.Button();
+            this.joinGame = new System.Windows.Forms.Button();
+            this.games = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // genGame
+            // 
+            this.genGame.Location = new System.Drawing.Point(90, 433);
+            this.genGame.Name = "genGame";
+            this.genGame.Size = new System.Drawing.Size(284, 100);
+            this.genGame.TabIndex = 0;
+            this.genGame.Text = "crea partita";
+            this.genGame.UseVisualStyleBackColor = true;
+            // 
+            // joinGame
+            // 
+            this.joinGame.Location = new System.Drawing.Point(481, 433);
+            this.joinGame.Name = "joinGame";
+            this.joinGame.Size = new System.Drawing.Size(298, 100);
+            this.joinGame.TabIndex = 1;
+            this.joinGame.Text = "entra la partita";
+            this.joinGame.UseVisualStyleBackColor = true;
+            // 
+            // games
+            // 
+            this.games.FormattingEnabled = true;
+            this.games.Location = new System.Drawing.Point(481, 70);
+            this.games.Name = "games";
+            this.games.Size = new System.Drawing.Size(298, 355);
+            this.games.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 581);
+            this.Controls.Add(this.games);
+            this.Controls.Add(this.joinGame);
+            this.Controls.Add(this.genGame);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button genGame;
+        private System.Windows.Forms.Button joinGame;
+        private System.Windows.Forms.ListBox games;
+    }
+}
+
---
+++ client/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient(); 
+        NetworkStream serverStream;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Client Started");
+            try {
+                clientSocket.Connect("10.10.21.29", 11000);
+                MessageBox.Show("Client Socket Program - Server Connected ...");
+
+                serverStream = clientSocket.GetStream();
+
+                byte[] outStream = System.Text.Encoding.ASCII.GetBytes("ciao sono vano :D<EOF>");
+
+                serverStream.Write(outStream, 0, outStream.Length);
+
+                serverStream.Flush();
+
+                byte[] inStream = new byte[(int)clientSocket.ReceiveBufferSize];
+
+                serverStream.Read(inStream, 0, (int)clientSocket.ReceiveBufferSize);
+
+                string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+
+                MessageBox.Show("Data from Server : " + returndata);
+            } catch (SocketException) {
+                MessageBox.Show("oki non funziona ;-;");
+            }
+        }
+    }
+}
